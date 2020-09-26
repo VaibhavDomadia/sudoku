611154a48c75dd96bf0bc2bd7e3ea8c9b0c8c26e
Vaibhav <vjdomadia@gmail.com>
2020-09-26 22:26:01 +0530

    Add Header Component

---
+++ src/Components/Header/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './header.css';
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>SUDOKU</div>
+        );
+    }
+}
+
+export default Header;
