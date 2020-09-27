e223b6a1309edfc2010f15898a0ce762446cfbe9
Vaibhav <vjdomadia@gmail.com>
2020-09-27 17:05:01 +0530

    Add Row Component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/header';
-import Cell from './Components/Cell/cell';
+import Row from './Components/Row/row';
 
 class App extends React.Component {
     constructor(props) {
@@ -13,7 +13,7 @@ class App extends React.Component {
         return (
             <div>
                 <Header/>
-                <Cell/>
+                <Row/>
             </div>
         )
     }
---
+++ src/Components/Row/row.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './row.css';
+
+class Row extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>row</div>
+        );
+    }
+}
+
+export default Row;
