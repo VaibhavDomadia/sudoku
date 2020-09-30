ec9d88fb4c128b2dbfce732c8038a118a5ae3fac
Vaibhav <vjdomadia@gmail.com>
2020-10-01 00:08:59 +0530

    Updates

---
+++ precache-manifest.4f854a68c3fdc096881c155a32032952.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd272037eba0430586066f0f4e241e2e",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "bd2ce2cfc3489e92be58",
+    "url": "/sudoku/static/css/main.8758feb8.chunk.css"
+  },
+  {
+    "revision": "7dcbc5e73b7adef2536e",
+    "url": "/sudoku/static/js/2.efc305a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sudoku/static/js/2.efc305a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd2ce2cfc3489e92be58",
+    "url": "/sudoku/static/js/main.b0b75f8f.chunk.js"
+  },
+  {
+    "revision": "8d8e892f47ac5d31712b",
+    "url": "/sudoku/static/js/runtime-main.f7976529.js"
+  },
+  {
+    "revision": "a2594cecc10060ea97781a70ee5125d5",
+    "url": "/sudoku/static/media/githubicon.a2594cec.svg"
+  },
+  {
+    "revision": "fbd6c8eaa59ce024097bb25bcb822fba",
+    "url": "/sudoku/static/media/hide.fbd6c8ea.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sudoku/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1e7b53c163614e15b5626d275abb5fe2",
+    "url": "/sudoku/static/media/new.1e7b53c1.svg"
+  },
+  {
+    "revision": "a5b1200bfb73ba36f210acdd76bc0177",
+    "url": "/sudoku/static/media/reset.a5b1200b.svg"
+  },
+  {
+    "revision": "ad95b87ba04a2a188cd3a3dc5d328910",
+    "url": "/sudoku/static/media/visible.ad95b87b.svg"
+  }
+]);
